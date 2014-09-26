cdeed44708095db1c3fb7fda2d5839f59b20e079
saginawj <saginawj@gmail.com>
2014-09-26 05:56:37 -0400

    Added  the second method

--- jms-Alabama/src/hello.java
+++ jms-Alabama/src/hello.java
@@ -3,4 +3,8 @@ public class hello {
 	public static void main(String[] args){
 		System.out.println("Hello Jon");
 	}
+	
+	public static void secondMethod(String[] args){
+		System.out.println("second");
+	}
 }
