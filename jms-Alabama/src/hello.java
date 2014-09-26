54f94f3606d7497658014dbfb973b3a965a469fa
saginawj <saginawj@gmail.com>
2014-09-26 06:32:55 -0400

    added a third method

--- jms-Alabama/src/hello.java
+++ jms-Alabama/src/hello.java
@@ -7,4 +7,7 @@ public class hello {
 	public static void secondMethod(String[] args){
 		System.out.println("second");
 	}
+	public static void thirdMethod(String[] args){
+		System.out.println("third");
+	}
 }
