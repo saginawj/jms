d03d6e9ca3c2b7671a98bef1afa9687a44c22731
saginawj <saginawj@gmail.com>
2014-09-27 13:19:34 -0400

    added unit test

---
+++ jms-Arkansas/src/Fake.java
@@ -0,0 +1,20 @@
+
+public class Fake {
+
+	public Fake() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Fake Method");
+	}
+	public static string arkansas() {
+		string strArkansas = "Bad State";
+		return strArkansas;
+	}
+
+}
