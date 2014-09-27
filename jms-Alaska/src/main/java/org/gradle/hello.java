2471359553146401f390b953e765415e66e886e7
saginawj <saginawj@gmail.com>
2014-09-27 04:43:28 -0400

    first commit of Gradle project

---
+++ jms-Alaska/src/main/java/org/gradle/hello.java
@@ -0,0 +1,15 @@
+package org.gradle;
+
+public class hello {
+	public static void main(String[] args){
+		System.out.println("Hello Jon");
+	}
+	
+	public static void secondMethod(String[] args){
+		System.out.println("second");
+	}
+	public static void thirdMethod(String[] args){
+		System.out.println("third");
+	}
+
+}
