0bb2d7ae096b6270e2e313ae5d44376fb039d519
saginawj <saginawj@gmail.com>
2014-09-27 13:30:01 -0400

    Two junit tests

---
+++ jms-California/src/api/java/doubler/Giant.java
@@ -0,0 +1,15 @@
+package doubler;
+
+public class Giant {
+
+	public Giant() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String Homerun() {
+		String strHomerun = "grand slam";
+		
+		return strHomerun;
+	}
+
+}
---
+++ jms-California/src/api/java/doubler/GiantTest.java
@@ -0,0 +1,26 @@
+package doubler;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class GiantTest {
+
+	@Test
+	public void testGiant() {
+		//fail("Not yet implemented");
+		boolean condition = true;
+		Assert.assertTrue(condition);
+	
+	}
+
+	@Test
+	public void testHomerun() {
+		//fail("Not yet implemented");
+		boolean condition = true;
+		Assert.assertTrue(condition);
+		
+	}
+
+}
